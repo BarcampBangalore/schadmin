9f99031bef003ee67ee41ffdca28fb98237acf3f
Aman <amanmanglik@gmail.com>
2014-10-12 02:15:51 +0530

    update the links for node related pushes

--- links.php
+++ links.php
@@ -7,10 +7,10 @@
             <a target="_blank" href="web_json.php">Web JSON</a><br>
             <a target="_blank" href="web_json.php?mode=pre">Web JSON pre-event</a><br>
             <a target="_blank" href="http://barcampbng.appspot.com/prepMessage">Android send Notification</a><br>
-            <a target="_blank" href="http://bcb.ap01.aws.af.cm/">Node tweets</a><br>
-            <a target="_blank" href="http://bcb.ap01.aws.af.cm/schedule">Node schedule</a><br>
-            <a target="_blank" href="http://bcb.ap01.aws.af.cm/update">Node send update</a><br>
-            <a target="_blank" href="http://bcb.ap01.aws.af.cm/push">Node refresh schedule</a><br>
+            <a target="_blank" href="http://barcampbangalore.org:3000/">Node tweets</a><br>
+            <a target="_blank" href="http://barcampbangalore.org:3000/schedule">Node schedule</a><br>
+            <a target="_blank" href="http://barcampbangalore.org:3000/update">Node send update</a><br>
+            <a target="_blank" href="http://barcampbangalore.org:3000/push">Node refresh schedule</a><br>
 
 
         </div>
